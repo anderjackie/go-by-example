5d1548211efde33d21065a64725a23bfefbde0a5
Anderson Moreira Silva <anderjackie7@gmail.com>
2023-03-31 09:45:37 -0300

    Reasoning non-blocking channel operations, closing channels and range over channels

--- 22-channels/channel-synchronization.go
+++ 22-channels/channel-synchronization.go
@@ -15,10 +15,9 @@ func worker(done chan bool) {
 
 func main() {
   done := make(chan bool, 1)
-
   go worker(done)
 
   // This blocks the execution of the program until done receives a value
-  // If this block operation is not here the code will execute the worker goroutine and finish so th result will never be seen
+  // If this block operation is not here the code will execute the worker goroutine and finish so the result will never be seen
   <-done
 }
